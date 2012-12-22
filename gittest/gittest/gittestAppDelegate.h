ed1e9ed516155511c4f74cd532c54863233ed499
picasso <ayuzjd@gmail.com>
2012-12-22 13:44:42 +0800

    1

---
+++ gittest/gittest/gittestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gittestAppDelegate.h
+//  gittest
+//
+//  Created by z picasso on 12-12-22.
+//  Copyright (c) 2012年 z picasso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gittestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
---
+++ gittest/gittest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by z picasso on 12-12-22.
+//  Copyright (c) 2012年 z picasso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
